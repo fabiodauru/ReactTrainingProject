0a20d246ed2237993a7d674197a72bc71a35b564
ADS\fada <fabio.dauru@zuhlke.com>
2025-09-29 13:06:08 +0200

    routing setup

---
+++ trainingProject.fe/src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+export default function ErrorPage(){
+    
+    
+    
+    return(
+        <div className="">
+            <p>An Error occured please try again later</p>
+        </div>
+    )
+}
