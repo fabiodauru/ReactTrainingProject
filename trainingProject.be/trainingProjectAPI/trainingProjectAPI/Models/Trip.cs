f7172b809fab59f08ea50d22abff2e7fbb77bc95
Giulian Mazzeo <giulian.mazzeo@zuhlke.com>
2025-09-29 13:58:59 +0200

    Add properties to Trip model for enhanced trip details

--- trainingProject.be/trainingProjectAPI/trainingProjectAPI/Models/Trip.cs
+++ trainingProject.be/trainingProjectAPI/trainingProjectAPI/Models/Trip.cs
@@ -2,6 +2,17 @@ namespace trainingProjectAPI.Models
 {
     public class Trip
     {
-        
+        public Guid Id { get; set; }
+        public required Coordinates StartCoordinates { get; set; }
+        public required Coordinates EndCoordinates { get; set; }
+        public required string TripName { get; set; }
+        public required Guid CreatedBy { get; set; }
+        public List<Image>? Images { get; set; }
+        public List<Restaurant>? Restaurants { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public double? Elevation { get; set; }
+        public double? Distance { get; set; }
+        public int? Difficulty { get; set; }
+        public string? Description { get; set; }
     }
 }
