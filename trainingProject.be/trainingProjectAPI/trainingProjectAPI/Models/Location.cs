f67c2c37f1cb6d526c918f5185ad2bf7c6037b65
Giulian Mazzeo <giulian.mazzeo@zuhlke.com>
2025-09-29 14:03:11 +0200

    Add Address and Coordinates properties to Location model

--- trainingProject.be/trainingProjectAPI/trainingProjectAPI/Models/Location.cs
+++ trainingProject.be/trainingProjectAPI/trainingProjectAPI/Models/Location.cs
@@ -2,6 +2,7 @@ namespace trainingProjectAPI.Models
 {
     public class Location
     {
-        
+        public Address? Address { get; set; }
+        public required Coordinates Coordinates { get; set; }
     }
 }
