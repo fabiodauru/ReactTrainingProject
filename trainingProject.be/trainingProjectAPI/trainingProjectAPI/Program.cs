7a294b59a56ed4db3408edb43318a5db0ddf1678
ADS\fada <fabio.dauru@zuhlke.com>
2025-10-01 08:33:03 +0200

    Removed unnecessary default things from Program.cs and added Deppendency-Injection for the persistencyService

--- trainingProject.be/trainingProjectAPI/trainingProjectAPI/Program.cs
+++ trainingProject.be/trainingProjectAPI/trainingProjectAPI/Program.cs
@@ -1,8 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using trainingProjectAPI.Interfaces;
+using trainingProjectAPI.PersistencyService;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var config = builder.Configuration;
+var rootPath = Directory.GetCurrentDirectory();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+config.SetBasePath(rootPath);
+config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+services.AddHttpClient();
+
+services.AddSingleton(config);
+services.AddSingleton<IPersistencyService, MongoDbContext>();
+
+services.AddControllers();
+
+
+
+services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -10,32 +30,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
