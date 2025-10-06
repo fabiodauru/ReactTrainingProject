fcd35a93c6bc5015c1714ef8b5f3efdf0d81fe21
ADS\elku <elia.kuster@zuhlke.com>
2025-10-06 13:42:56 +0200

    Created Login

--- trainingProject.fe/src/Router.tsx
+++ trainingProject.fe/src/Router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
+import LoginPage from "./pages/LoginPage.tsx";
 
 export default function Router() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="" element={<HomePage/>} />
+                <Route path="/login" element={<LoginPage />} ></Route>
                 <Route errorElement={<ErrorPage/>}/>
                 <Route path="/*" element={<ErrorPage/>} />
             </Routes>
---
+++ trainingProject.fe/src/pages/LoginPage.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+
+export default function LoginPage(){
+    const navigate = useNavigate();
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginFailedMessage, setLoginFailedMessage] = useState("");
+    
+    const handleSubmit =async (event: React.FormEvent) => {
+        event.preventDefault()
+        
+        const response = await fetch("http://localhost:5065/api/Authenticate/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username: username, password: password })
+        });
+        const data = await response.json();
+        
+        if (response.status === 200 && data.message === "Success") 
+        {
+            localStorage.setItem("username", data.username);
+            localStorage.setItem("token", data.token);
+            navigate("/");
+        }
+        else{
+            setLoginFailedMessage(data.Message);
+        }
+    }
+    
+    return (
+        <>
+            <div className="login-container">
+                <div className="login-header">
+                    <h1>TravelDingsBums</h1>
+                    <p>Login</p>
+                </div>
+                
+                <form onSubmit={handleSubmit} className="login-form">
+                    <div className="form-group">
+                        <label htmlFor="inputUsername">Username</label>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            id="inputUsername"
+                            onChange={(event) => setUsername(event.target.value)}
+                            value={username}
+                            required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputPassword">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            id="inputPassword"
+                            onChange={(event) => setPassword(event.target.value)}
+                            value={password}
+                            required/>
+                    </div>
+                    
+                    <button type="submit" className="login-btn">Login</button>
+                    
+                    <div className="error-message">
+                        {loginFailedMessage}
+                    </div>
+                    
+                    <div className="login-footer">
+                        <p>Don't have an account? <a href="/register">Register here</a></p>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
