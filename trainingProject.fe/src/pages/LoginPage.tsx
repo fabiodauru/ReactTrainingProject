1ec5d0902b0ced9b4bf3804352ed3e6f1a18e493
Giulian Mazzeo <giulian.mazzeo@zuhlke.com>
2025-10-07 16:08:08 +0200

    Enhance LoginPage styling: improve layout and add responsive design elements

--- trainingProject.fe/src/pages/LoginPage.tsx
+++ trainingProject.fe/src/pages/LoginPage.tsx
@@ -20,23 +20,33 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username/Email"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="justify-center items-center flex flex-col h-screen w-full">
+      <h2 className="text-3xl font-bold mb-8">Login Page</h2>
+
+      <div className="p-8 shadow-lg border rounded-2xl flex flex-col w-80 bg-white/20 backdrop-blur-md">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="border border-gray-300 p-2 rounded w-full mb-4"
+            type="text"
+            placeholder="Username/Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="border border-gray-300 p-2 rounded w-full mb-6"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
