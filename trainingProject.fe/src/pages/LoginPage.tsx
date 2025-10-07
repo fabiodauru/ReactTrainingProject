876f00cb40c4bf94b79dc4de6578f8f9f8eb120b
ADS\elku <elia.kuster@zuhlke.com>
2025-10-07 07:59:33 +0200

    Matheo schon gemacht

--- trainingProject.fe/src/pages/LoginPage.tsx
+++ trainingProject.fe/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
+//Hat de El Tony Mathe Theo schon gemacht
 
 export default function LoginPage(){
     const navigate = useNavigate();
