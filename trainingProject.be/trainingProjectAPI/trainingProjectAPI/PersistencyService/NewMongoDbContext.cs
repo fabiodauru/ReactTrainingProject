7e9aa3bd6b65789f82a65a4198adc4fa9cf40b9d
ADS\elku <elia.kuster@zuhlke.com>
2025-10-28 09:38:10 +0100

    refactor whole UserService

--- trainingProject.be/trainingProjectAPI/trainingProjectAPI/PersistencyService/NewMongoDbContext.cs
+++ trainingProject.be/trainingProjectAPI/trainingProjectAPI/PersistencyService/NewMongoDbContext.cs
@@ -141,7 +141,7 @@ public class NewMongoDbContext
     }
     
     
-    public async Task<T?> FindByProperty<T>(string property, object value) where T : IHasId
+    public async Task<List<T>?> FindByProperty<T>(string property, object value) where T : IHasId
     {
         try
         {
@@ -152,7 +152,7 @@ public class NewMongoDbContext
             var collection = _database.GetCollection<T>(typeof(T).Name + _collectionSuffix);
             var filter = Builders<T>.Filter.Eq(property, value);
             var response = await collection.FindAsync(filter);
-            var result = await response.FirstOrDefaultAsync();
+            var result = await response.ToListAsync();
             _logger.LogInformation($"Find {property} in {typeof(T).Name + _collectionSuffix}");
             return result;
         }
--- trainingProject.be/trainingProjectAPI/trainingProjectAPI/Services/NewUserService.cs
+++ trainingProject.be/trainingProjectAPI/trainingProjectAPI/Services/NewUserService.cs
@@ -36,7 +36,8 @@ public class NewUserService
             {
                 throw new ValidationException("Attempt to login as Sentiel");
             }
-            var response = await _context.FindByProperty<User>("Username",  username);
+
+            var response = (await _context.FindByProperty<User>("Username", username))?.SingleOrDefault();
             if (response == null || _hasher.VerifyHashedPassword(response, response.Password, password) == PasswordVerificationResult.Failed)
             {
                 throw new NotFoundException("User not found");
@@ -58,7 +59,6 @@ public class NewUserService
             if (string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password))
             { 
                 throw new ValidationException("Username or Password is empty");
-
             }
             if (user.Username == "Sentiel")
             { 
@@ -85,12 +85,7 @@ public class NewUserService
     {
         try
         {
-            var userToUpdate = await _context.FindByIdAsync<User>(id);
-            if (userToUpdate != null)
-            {
-                throw new NotFoundException("User not found");
-            }
-
+            var userToUpdate = await _context.FindByIdAsync<User>(id) ?? throw new NotFoundException("User not found");
             foreach (var prop in typeof(User).GetProperties())
             {
                 if (prop.Name == nameof(User.Password))
@@ -98,12 +93,7 @@ public class NewUserService
                     newUser.Password = _hasher.HashPassword(newUser, newUser.Password);
                 }
             }
-
-            var response = await _context.UpdateAsync(id, newUser);
-            if (response == null)
-            {
-                throw new ConflictException("Updating user failed");
-            }
+            var response = await _context.UpdateAsync(userToUpdate.Id, newUser) ?? throw new ConflictException("Updating user failed");
             _logger.LogInformation($"User {response.Username} updated");
             return response;
         }
@@ -118,11 +108,7 @@ public class NewUserService
     {
         try
         {
-            var response = await _context.FindByIdAsync<User>(userId);
-            if (response == null)
-            {
-                throw new NotFoundException("User not found");
-            }
+            var response = await _context.FindByIdAsync<User>(userId) ??  throw new NotFoundException("User not found");
             _logger.LogInformation($"User {response.Username} found");
             return response;
         }
@@ -132,4 +118,127 @@ public class NewUserService
             throw;
         }
     }
+    
+    public async Task DeleteUserAsync(Guid userId)
+    {
+        try
+        {
+            var sentielId = await GetSentielIdAsync();
+            var trips = await _context.FindByProperty<Trip>("CreatedBy", userId) ?? throw new NotFoundException("Trips not found");
+            var sentiel = await _context.FindByIdAsync<User>(sentielId) ?? throw new NotFoundException("Sentiel not found");
+            foreach (var trip in trips)
+            {
+                var responseTrip = await _context.FindAndUpdateByPropertyAsync<Trip>(trip.Id, "CreatedBy", sentielId) ?? throw new ConflictException("Trip not updated");
+                sentiel.Trips!.Add(responseTrip.Id);
+            }
+
+            var responseUser = await _context.FindAndUpdateByPropertyAsync<User>(sentielId, "Trips", trips) ?? throw new ConflictException("Sentiel not updated");
+            sentiel = responseUser;
+
+            await _context.DeleteAsync<User>(userId);
+            _logger.LogInformation($"User {userId} deleted and trips saved in {sentiel.Username}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error deleting user {UserId}.", userId);
+            throw;
+        }
+    }
+
+    public async Task<User> UpdateUserAsync(Guid userId, string property, object value)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(property))
+            {
+                throw new ValidationException("Property name is empty");
+            }
+            var response = await _context.FindAndUpdateByPropertyAsync<User>(userId, property, value) ?? throw new NotFoundException("User not found");
+            _logger.LogInformation($"User {response.Username} updated");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error updating user {UserId}.", userId);
+            throw;
+        }
+    }
+
+    public async Task<User> GetUserByProperty(string property, object value)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(property))
+            {
+                throw new ValidationException("Property name is empty");
+            }
+
+            var response =
+                (await _context.FindByProperty<User>(property, value) ?? throw new NotFoundException("User not found"))
+                .SingleOrDefault();
+            if (response == null)
+            {
+                throw new NotFoundException("User not found");
+            }
+
+            _logger.LogInformation($"User {response.Username} found");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting user by {Property}: {Value}",  property, value.ToString());
+            throw;
+        }
+    }
+    
+    private async Task<Guid> GetSentielIdAsync()
+    {
+        if (_sentielId != Guid.Empty)
+        {
+            return _sentielId;
+        }
+        var sentinel = await CreateSentielIfNotExistsAsync();
+        _sentielId = sentinel.Id;
+        return _sentielId;
+    }
+    
+    public async Task<User> CreateSentielIfNotExistsAsync()
+    {
+        try
+        {
+            var sentiel = (await _context.FindByProperty<User>("Username", "Sentiel"))!.FirstOrDefault();
+            if (sentiel != null)
+            {
+                return sentiel;
+            }
+            User sentinel = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "Sentiel",
+                Email = "sentinel@system.local",
+                Password = "sentiel",
+                UserFirstName = "Sentiel",
+                UserLastName = "Sentiel",
+                Birthday = DateOnly.FromDateTime(DateTime.Now.AddYears(-1)),
+                Address = new Address
+                {
+                    Street = "Sentiel Street",
+                    City = "Sentiel City",
+                    ZipCode = "00000",
+                    Country = "Sentiel Country"
+                }
+            };
+            _sentielId = sentinel.Id;
+            sentinel.Password = _hasher.HashPassword(sentinel, sentinel.Password);
+
+            var response = await _context.CreateAsync(sentinel);
+            _logger.LogInformation("Sentiel user created.");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating sentiel.");
+            throw;
+        }
+    } 
 }
