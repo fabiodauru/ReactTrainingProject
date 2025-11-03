765b6ca28dc6d0ccd7820f8a5e25bc57d087bd69
Giulian Mazzeo <giulian.mazzeo@zuhlke.com>
2025-11-03 16:58:24 +0100

    Refactor UserContext: replace username state with user object, streamline user fetching logic, and enhance context value

--- trainingProject.fe/src/context/UserContext.tsx
+++ trainingProject.fe/src/context/UserContext.tsx
@@ -4,25 +4,29 @@ import type { User } from "@/lib/type";
 import { createContext, useContext, useEffect, useState } from "react";
 
 interface UserContextType {
-  username: string | null;
+  user: User | null;
 }
 
 const UserContext = createContext<UserContextType | null>(null);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
-  const [username, setUsername] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    api
-      .get<User>(`${ENDPOINTS.USER.ME}`)
-      .then((user) => {
-        setUsername(user.username);
-      })
-      .catch(() => setUsername(null));
+    fetchUser();
   }, []);
 
+  const fetchUser = async () => {
+    try {
+      const userData = await api.get<User>(ENDPOINTS.USER.ME);
+      setUser(userData);
+    } catch {
+      setUser(null);
+    }
+  };
+
   return (
-    <UserContext.Provider value={{ username }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
   );
 }
 
