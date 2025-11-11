86c5f86e0f1856048554827700ce9a6db8374425
ADS\fada <fabio.dauru@zuhlke.com>
2025-11-11 08:51:26 +0100

    Fixed shadcn input-group import issues on the dev

---
+++ trainingProject.fe/src/components/SearchUserComponent.tsx
@@ -0,0 +1,110 @@
+import { Search, X } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "../components/ui/input-group";
+import { useEffect, useState } from "react";
+import DefaultPFP from "../assets/Default_pfp.svg";
+
+import type { User } from "../lib/type";
+
+export default function SearchUserComponent({
+  searchIsOpen,
+  setSearchIsOpen,
+}: {
+  searchIsOpen: boolean;
+  setSearchIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchedUsers, setSearchedUsers] = useState<User[]>([]);
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  useEffect(() => {
+    var searchChanges: User[] = [];
+    users.forEach((user) => {
+      if (
+        user.username
+          .toLocaleLowerCase()
+          .includes(searchInput.toLocaleLowerCase())
+      ) {
+        searchChanges.push(user);
+      }
+    });
+    setSearchedUsers(searchChanges);
+  }, [users, searchInput]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        var res = await fetch("http://localhost:5065/api/User", {
+          credentials: "include",
+        });
+        var data = await res.json();
+
+        var recivedUsers = Array.isArray(data) ? (data as User[]) : [];
+
+        recivedUsers.forEach((user) => {
+          if (user.profilePictureUrl == null) {
+            user.profilePictureUrl = DefaultPFP;
+          }
+        });
+
+        setUsers(recivedUsers);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  const handleSearch = () => {
+    setSearchIsOpen(true);
+  };
+
+  if (users.length == 0) return <p>Loading Users...</p>;
+
+  return (
+    <>
+      <div className="relative w-full max-w-4xl text-[var(--color-muted-foreground)]">
+        <InputGroup>
+          <InputGroupInput
+            placeholder="theRealOtto"
+            onFocus={handleSearch}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+            }}
+            value={searchInput}
+          />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+          <X
+            onClick={() => setSearchInput("")}
+            className="h-[1.1rem] pr-2 text-[var(--color-muted-foreground)]"
+          />
+        </InputGroup>
+        {searchIsOpen ? (
+          <div className="absolute w-full h-[50vh] bg-[color:var(--color-secondary-accent)] rounded-b-xl overflow-auto text-[color:var(--color-foreground)]">
+            <ul>
+              {searchedUsers.map((user) => (
+                <li
+                  key={user.username}
+                  className="p-2f flex justify-start items-center gap-3 p-2"
+                >
+                  <img
+                    src={user.profilePictureUrl}
+                    alt="pfp"
+                    className="h-[3rem] rounded-full"
+                  />
+                  <a href={"./socialMedia/User/" + user.username}>
+                    {user.username}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+}
